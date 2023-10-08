2ba4cf7533ac1a3f6e27b235656b3fb815714c04
Alex Rosegrid <rosegrid58@gmail.com>
2023-10-08 13:21:45 +0530

    migrate to init.lua for neovim

---
+++ .dotfiles/.config/nvim/init.lua
@@ -0,0 +1,79 @@
+vim.opt.termguicolors = true
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.ignorecase = true
+vim.opt.wrap = true
+vim.opt.showtabline = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.cmdheight=0
+
+vim.loader.enable()
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require('plugins')
+
+vim.cmd([[
+
+    syntax on
+    filetype plugin indent on
+
+    augroup FileTypeOptions
+        autocmd!
+        autocmd BufNewFile,BufRead *.sasm setfiletype asm
+        autocmd BufNewFile,BufRead *.bf setfiletype text
+        autocmd FileType markdown noremap <F6> :MarkdownPreview <CR>
+        autocmd FileType asm set tabstop=24 shiftwidth=24 softtabstop=24
+    augroup END
+
+    augroup ZshHighlighting
+        autocmd!
+        autocmd BufNewFile,BufRead .zalias setfiletype sh
+        autocmd BufNewFile,BufRead .zfunctions setfiletype sh
+    augroup END
+
+    augroup LaTeX
+        autocmd!
+        "compile latex
+        autocmd FileType tex noremap ztx :!latexmk -quiet -pdf -auxdir=$HOME/.texbuild/ '%:t' 1>/dev/null 2>&1 <CR> <CR>
+    augroup END
+
+    augroup Folding
+        autocmd!
+        set foldmethod=expr
+        set foldexpr=nvim_treesitter#foldexpr()
+        autocmd BufEnter * normal zR
+    augroup END
+
+]])
+
+local key_opts = {noremap = true,}
+
+vim.keymap.set('i', '(', "()<Esc>i", key_opts)
+vim.keymap.set('i', '[', "[]<Esc>i", key_opts)
+vim.keymap.set('i', '{', "{}<Esc>i", key_opts)
+vim.keymap.set('i', "''", "''<Esc>i", key_opts)
+vim.keymap.set('i', '"', '""<Esc>i', key_opts)
+
+vim.keymap.set('i', "{<cr>", "{<cr>}<Esc>O", key_opts)
+vim.keymap.set('i', "-[", "-[[<Esc>a", key_opts)
+vim.keymap.set('i', "-]", "-]]<Esc>a", key_opts)
+
+-- vim.keymap.set('n', "jj", "<Esc>", key_opts)
+vim.keymap.set('n', "zz", ":noh<cr>", key_opts)
+
+vim.keymap.set('n', "<F4>", ":Telescope live_grep <cr>", key_opts)
+
