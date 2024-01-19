a865ef94e4ad3e22cee87d5da1a9508447b797a5
Alex Rosegrid <rosegrid58@gmail.com>
2024-01-19 12:12:28 +0530

    refine tex to pdf keybind for neovim

---
+++ .dotfiles/.config/nvim/lua/keys.lua
@@ -0,0 +1,19 @@
+local key_opts = {noremap = true,}
+
+local function imap(key, cmd)
+    vim.keymap.set('i', key, cmd, key_opts)
+end
+
+imap('(',       "()<Esc>i")
+imap('[',       "[]<Esc>i")
+imap('{',       "{}<Esc>i")
+imap("''",      "''<Esc>i")
+imap('""',      '""<Esc>i')
+imap("{<cr>",   "{<cr>}<Esc>O")
+imap("-[",      "-[[<Esc>a")
+imap("-]",      "-]]<Esc>a")
+
+-- vim.keymap.set('i', "jj", "<Esc>", key_opts)
+vim.keymap.set('n', "zz", ":noh<cr>", key_opts)
+
+vim.keymap.set('n', "<F4>", ":Telescope live_grep <cr>", key_opts)
--- .dotfiles/.config/nvim/lua/options.lua
+++ .dotfiles/.config/nvim/lua/options.lua
@@ -8,5 +8,6 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
-vim.opt.cmdheight=0
+vim.opt.cmdheight = 0
+vim.opt.foldmethod = "manual"
 
