a4dea13f5dc4038e4bc515e731f1f14d8f6177f5
Alex Rosegrid <rosegrid58@gmail.com>
2024-01-27 17:52:50 +0530

    .

--- .dotfiles/.config/nvim/after/ftplugin/tex.lua
+++ .dotfiles/.config/nvim/after/ftplugin/tex.lua
@@ -3,10 +3,12 @@ local FSHOME = vim.fn.expand("$HOME")
 local texInfo = {}
 -- all tex files are stored in the $HOME/.authoring/<projectName>/ directory
 _, _, texInfo.projectName = string.find(vim.fn.expand('%:~'), "authoring/([^/]*)/")
+_, _, texInfo.fileName = string.find(vim.fn.expand('%:t'), "([^.]*)")
 texInfo.rootDir = FSHOME .. "/.authoring/" .. texInfo.projectName
 texInfo.outDir = texInfo.rootDir .. "/pdf/"
 texInfo.auxDir = FSHOME .. "/.texbuild/"
 texInfo.target = vim.fn.expand('%:.')
+texInfo.pdf = texInfo.outDir .. texInfo.fileName .. ".pdf"
 
 
 -- make pdf from tex file; intended for single tex files, with no input statements
@@ -21,6 +23,9 @@ vim.keymap.set(
     'n', "<space><space>c", get_pdf_make_command(), { noremap = true, silent = true, }
 )
 vim.keymap.set( 'n', "<space><space>m", ":!cd .. && mktex<CR>", { noremap = true, } )
+vim.keymap.set(
+    'n', "<space><space>z", ":!zathura " .. texInfo.pdf .. "&<CR><CR>", { noremap = true, }
+)
 
 vim.keymap.set( 'n', "<space><space>f", ":set filetype=tex<CR>", { noremap = true, } )
 vim.keymap.set( 'n', "<space>tt", ":b 1 | set filetype=tex<CR>", { noremap = true, } )
