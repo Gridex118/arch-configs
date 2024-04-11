25766d97167a76d44d83444184847c301e604f9f
Alex Rosegrid <rosegrid58@gmail.com>
2024-04-11 10:41:40 +0530

    .

--- .dotfiles/.config/eww/scripts/wifi.sh
+++ .dotfiles/.config/eww/scripts/wifi.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-if [[ "$1" == "--update" ]]; then # redundent
-    wifiname=$(iwgetid -r)
-    echo $wifiname
-    if  [ $(iwgetid -r) ]; then
-        eww update wifi-icon="直"
-        eww update wifi-text="Connected"
-    else
-        eww update wifi-icon="睊"
-        eww update wifi-text="Disconnected"
-    fi
+if  [ $(iwgetid -r) ]; then
+    eww update wifi-icon="直"
+    eww update wifi-text="Connected"
+else
+    eww update wifi-icon="睊"
+    eww update wifi-text="Disconnected"
 fi
+
+wifiname=$(iwgetid -r)
+echo $wifiname
