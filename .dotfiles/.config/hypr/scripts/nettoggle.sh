eb054a263a777770eb2eb9a2a3162184b7b35d64
Alex Rosegrid <rosegrid58@gmail.com>
2024-04-11 10:33:57 +0530

    Add nettoggle script

---
+++ .dotfiles/.config/hypr/scripts/nettoggle.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+LOG_FILE=/tmp/nettoggle.log
+
+WLAN_STATUS=$(awk '{print $4}' <(rfkill | grep wlan))
+if [ $WLAN_STATUS = "blocked" ]; then
+    rfkill unblock all
+fi
+unset WLAN_STATUS
+
+toggle_connection() {
+    SSID=$1
+    if [ -z "$(iw dev wlan0 link | grep $SSID)" ]; then
+        iwctl station wlan0 connect $SSID >> /dev/null 2>&1 || return 1
+        echo "connected to $SSID" > $LOG_FILE
+    else
+        iwctl station wlan0 disconnect
+        echo "disconnected from $SSID" > $LOG_FILE
+    fi
+    unset SSID
+    return 0
+}
+
+
+for ssid in `cat ~/.networks`; do
+    toggle_connection $ssid \
+    || echo "Could not connect to "$ssid > $LOG_FILE
+done
+
+unset LOG_FILE
