6dc5e024882291bbb24741a6eef74c8812107bcd
Alex Rosegrid <rosegrid58@gmail.com>
2024-04-12 20:21:31 +0530

    .

--- .dotfiles/.config/hypr/scripts/fullscreen.sh
+++ .dotfiles/.config/hypr/scripts/fullscreen.sh
@@ -1,5 +1,11 @@
 if [ -z "$(hyprctl binds | grep killactive)" ]; then
     hyprctl keyword bind ALT, Q, killactive
+    for w in {1..9}; do
+        hyprctl keyword bind ALT, $w, exec, hyprctl dispatch workspace $w
+    done
 else
     hyprctl keyword unbind ALT, Q
+    for w in {1..9}; do
+        hyprctl keyword unbind ALT, $w
+    done
 fi
