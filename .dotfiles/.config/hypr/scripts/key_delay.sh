f3a30be844859aeca15c19260eea92ecacd8ff7c
Alex Rosegrid <rosegrid58@gmail.com>
2024-04-21 10:47:27 +0530

    .

--- .dotfiles/.config/hypr/scripts/key_delay.sh
+++ .dotfiles/.config/hypr/scripts/key_delay.sh
@@ -3,13 +3,13 @@
 CURRENT_DELAY="`xset -q | grep "delay" | awk '{print $4}'`"
 
 hyprctl dismissnotify
-if [ $CURRENT_DELAY = "310" ]; then
+if [ $CURRENT_DELAY = "250" ]; then
     hyprctl notify -1 1500 "rgb(ffffff)" "fontsize:30 Turbo repeat"
     hyprctl keyword input:repeat_delay 85
     hyprctl keyword input:repeat_rate 25
 else
     hyprctl notify -1 1500 "rgb(ffffff)" "fontsize:30 Normal repeat"
-    hyprctl keyword input:repeat_delay 310
+    hyprctl keyword input:repeat_delay 250
     hyprctl keyword input:repeat_rate 30
 fi
 
