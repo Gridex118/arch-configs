31df7a9bce5fb0710f592465f561ad17fdd4f214
Alex Rosegrid <rosegrid58@gmail.com>
2024-04-29 19:54:26 +0530

    .

--- .dotfiles/.config/nvim/after/ftplugin/tex.lua
+++ .dotfiles/.config/nvim/after/ftplugin/tex.lua
@@ -3,15 +3,16 @@ local FSHOME = vim.fn.expand("$HOME")
 local texInfo = {}
 -- all tex projects are stored in the $HOME/authoring/<projectName>/ directory
 _, _, texInfo.projectName = string.find(vim.fn.expand('%:~'), "authoring/([^/]*)/")
+texInfo.auxDir = FSHOME .. "/.texbuild/"
+texInfo.target = vim.fn.expand('%:.')
 if not (texInfo.projectName == nil) then
     texInfo.isProject = true
     _, _, texInfo.fileName = string.find(vim.fn.expand('%:t'), "([^.]*)")
     texInfo.rootDir = FSHOME .. "/authoring/" .. texInfo.projectName
     texInfo.outDir = texInfo.rootDir .. "/pdf/"
-    texInfo.auxDir = FSHOME .. "/.texbuild/"
-    texInfo.target = vim.fn.expand('%:.')
     texInfo.pdf = texInfo.outDir .. texInfo.fileName .. ".pdf"
 else
+    texInfo.outDir = "pdf/"
     texInfo.isProject = false
 end
 
@@ -23,13 +24,10 @@ local function get_pdf_make_command()
     return ":!latexmk" .. options .. texInfo.target .. redirection .. "<CR>"
 end
 
+vim.keymap.set( 'n', "<space><space>c", get_pdf_make_command(), { noremap = true, silent = true, } )
 
-local function setup_commands()
-    vim.keymap.set(
-        'n', "<space><space>c", get_pdf_make_command(), { noremap = true, silent = true, }
-    )
-    vim.keymap.set( 'n', "<space><space>M", ":!cd .. && mktex<CR>", { noremap = true, } )
-    vim.keymap.set( 'n', "<space><space>m", ":!mktex<CR>", { noremap = true, } )
+if texInfo.isProject then
+    vim.keymap.set( 'n', "<space><space>m", ":!cd " .. texInfo.rootDir .. "&& mktex<CR>", { noremap = true, } )
     vim.keymap.set(
         'n', "<space><space>z", ":!zathura " .. texInfo.pdf .. "&<CR><CR>", { noremap = true, }
     )
@@ -38,8 +36,4 @@ local function setup_commands()
     vim.keymap.set( 'n', "<space>tt", ":b 1 | set filetype=tex<CR>", { noremap = true, } )
     vim.keymap.set( 'n', "<space>tn", ":bnext | set filetype=tex<CR>", { noremap = true, } )
     vim.keymap.set( 'n', "<space>tN", ":bprevious | set filetype=tex<CR>", { noremap = true, } )
-end
-
-if texInfo.isProject then
-    setup_commands();
 end
