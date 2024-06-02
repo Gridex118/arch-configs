ab6fde4e0ce519b853804042575a9f7eb46f7af8
Alex Rosegrid <rosegrid58@gmail.com>
2024-06-02 23:14:03 +0530

    .

--- .dotfiles/.config/eww/scripts/get-workspaces.sh
+++ .dotfiles/.config/eww/scripts/get-workspaces.sh
@@ -2,10 +2,8 @@
 
 spaces (){
     WORKSPACE_WINDOWS=$(hyprctl workspaces -j | jq 'map({key: .id | tostring, value: .windows}) | from_entries')
-    seq 1 10 | jq --argjson windows "${WORKSPACE_WINDOWS}" --slurp -Mc 'map(tostring) | map({id: ., windows: ($windows[.]//0)})'
+    seq 1 9 | jq --argjson windows "${WORKSPACE_WINDOWS}" --slurp -Mc 'map(tostring) | map({id: ., windows: ($windows[.]//0)})'
 }
 
 spaces
-socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
-spaces
-done
+socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do spaces; done
