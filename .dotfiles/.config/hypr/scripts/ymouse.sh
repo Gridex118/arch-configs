024ca3ad5fb3502f48e477564e606a5356f7bbd5
Alex Rosegrid <rosegrid58@gmail.com>
2024-07-17 22:10:09 +0530

    .

--- .dotfiles/.config/hypr/scripts/ymouse.sh
+++ .dotfiles/.config/hypr/scripts/ymouse.sh
@@ -14,28 +14,28 @@ fi
 
 MOUSE_BASE=10
 
+MOUSE_ACCEL=$(($MOUSE_ACCEL + 10))
+
 case "$1" in
     h|1)
         ydotool mousemove -- -$(($MOUSE_BASE + $MOUSE_ACCEL)) 0
-        MOUSE_ACCEL=$(($MOUSE_ACCEL + 10))
     ;;
     j|2)
         ydotool mousemove -- 0 $(($MOUSE_BASE + $MOUSE_ACCEL))
-        MOUSE_ACCEL=$(($MOUSE_ACCEL + 10))
     ;;
     k|3)
         ydotool mousemove -- 0 -$(($MOUSE_BASE + $MOUSE_ACCEL))
-        MOUSE_ACCEL=$(($MOUSE_ACCEL + 10))
     ;;
     l|4)
         ydotool mousemove -- $(($MOUSE_BASE + $MOUSE_ACCEL)) 0
-        MOUSE_ACCEL=$(($MOUSE_ACCEL + 10))
     ;;
     click)
         ydotool click 0x40
         ydotool click 0x80
     ;;
+    reset)
+        MOUSE_ACCEL=0
+    ;;
 esac
-
 
 echo $MOUSE_ACCEL > $ACCEL_LOG
--- .dotfiles/.config/nvim/syntax/hypr.vim
+++ .dotfiles/.config/nvim/syntax/hypr.vim
@@ -3,4 +3,4 @@ syntax match @identifier /^\$[^=]\+/
 syntax match VusualNC /=\&[^=]\+$/
 syntax match Comment /\s*#.\+$/
 syntax match @constructor /^[^{]\+ {\|\s*}/
-syntax match Keyword /\s*\(binde\|bindm\|bind\|submap\|source\)\s*=/
+syntax match Keyword /\s*\(bind.?\|submap\|source\)\s*=/
