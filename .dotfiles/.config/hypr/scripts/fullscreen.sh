e02b51814b8c398da48e3b42436c1636f4df9ae0
Alex Rosegrid <rosegrid58@gmail.com>
2024-07-20 12:29:54 +0530

    .

--- .dotfiles/.config/hypr/scripts/fullscreen.sh
+++ .dotfiles/.config/hypr/scripts/fullscreen.sh
@@ -2,7 +2,5 @@
 
 hyprctl dispatch fullscreen
 
-FULLSCR=`hyprctl activeworkspace| grep "hasfullscreen"| awk -F: '{print $2}'| tr -d ' '`
-if [ $FULLSCR = '1' ]; then
-    hyprctl dispatch submap fullscr
-fi
+FULLSCR=`hyprctl activeworkspace -j| jq ."hasfullscreen"`
+[ $FULLSCR = 'true' ] && hyprctl dispatch submap fullscr
